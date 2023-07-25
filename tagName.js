4e48f827e0f19fe51c1c0e5b49571cc0c7739377
AB1234567-PROG <super16abhishek>
2023-07-25 10:32:50 +0530

    FourthCommit

---
+++ tagName.js
@@ -0,0 +1,27 @@
+//GETELEMENTSBYCLASSNAME//
+ var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.backgroundColor='grey';
+}
+// items[2].style.backgroundColor="green";
+
+// for(var i=0; i < items.length; i++)
+// {
+//     items[i].style.fontWeight='bold';
+// }
+
+//GETELEMENTSBYTAGNAME//
+//  var li = document.getElementsByTagName('li');
+//  console.log(li);
+// console.log(li[1]);
+// li[1].textContent="Hello"
+// li[1].style.fontWeight="bold"
+// li[1].style.backgroundColor="blue"
+// li[4].textContent="Hi"
+
+for(var i=0; i<li.length; i++)
+{
+    li[i].style.backgroundColor='grey';
+}
