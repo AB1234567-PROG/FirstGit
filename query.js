d0e9274bf056f9f6e046d9302ff50c311807b096
AB1234567-PROG <super16abhishek>
2023-07-25 19:03:42 +0530

    FifthFile

---
+++ query.js
@@ -0,0 +1,40 @@
+// QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = "Hello World";
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "Send";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundcolor = 'green'
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden'
+
+// var titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent= "Hello";
+
+ var items = document.querySelectorAll('.list-group-item')
+ items[1].style.color= 'green';
+
+ var odd = document.querySelectorAll('li:nth-child(odd)');
+
+ for(var i=0; i<odd.length;i++)
+ {
+    odd[i].style.backgroundColor='green';
+ }
+
+
+
+
