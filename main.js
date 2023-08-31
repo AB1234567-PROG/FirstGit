249c1034bbe9f689b91cab2db4bb6089b88bd8f5
Abhishek Singh Chaudhary <79990244+AB1234567-PROG@users.noreply.github.com>
2023-08-31 17:58:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,125 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// GET REQUEST
+function getTodos() {
+  // axios({
+  //   method: "get",
+  //   url: "https://jsonplaceholder.typicode.com/todos",
+  //   params: {
+  //     _limit: 5,
+  //   },
+  // })
+  //   .then((res) => showOutput(res))
+  //   .catch((err) => console.log(err));
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems)
-
-// Add Item
-function addItem(e){
-    e.preventDefault();
-
-     // Get input value
-     var newItem = document.getElementById('item').value;
-
-     // Create new li element
-     var li = document.createElement('li');
-     // Add class
-     li.className = 'list-group-item';
-     // Add text node with input value
-     li.appendChild(document.createTextNode(newItem));
-
-     // Create del button element
-     var deletebtn = document.createElement('button');
-
-     // Add classes to del button
-     deletebtn.className = 'btn btn-danger btn-sm float-right delete';
-
-     deletebtn.appendChild(document.createTextNode('X'));
-
-     // Append button to li 
-     li.appendChild(deletebtn);
-
-     // Append li to list
-    itemList.appendChild(li);
-
-
-
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    .then((res) => showOutput(res))
+    .catch((err) => console.log(err));
 }
 
-// Remove item
-function removeItem(e){
-    if(e.target.classList.contains('delete'))
-    {
-        if(confirm('Are you Sure?')){
-            var li =e.target.parentElement;
-            itemList.removeChild(li);
-        }
-    }
-
-
+// POST REQUEST
+function addTodo() {
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", {
+      title: "New Todo",
+      completed: false,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.log(err));
 }
 
-// Filter Items
-function filterItems(e){
-    //convert text to lowercase
-    var text = e.target.value.toLowerCase();
-    console.log(text);
+// PUT/PATCH REQUEST
+function updateTodo() {
+  axios
+    .put("https://jsonplaceholder.typicode.com/todos/1", {
+      title: "Updated Todo",
+      completed: true,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
+
+// DELETE REQUEST
+function removeTodo() {
+  axios
+    .delete("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// SIMULTANEOUS DATA
+function getData() {
+  console.log("Simultaneous Request");
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
+  console.log("Custom Headers");
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
+  console.log("Transform Response");
+}
+
+// ERROR HANDLING
+function errorHandling() {
+  console.log("Error Handling");
+}
+
+// CANCEL TOKEN
+function cancelToken() {
+  console.log("Cancel Token");
+}
+
+// INTERCEPTING REQUESTS & RESPONSES
+
+// AXIOS INSTANCES
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById("res").innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+
+// Event listeners
+document.getElementById("get").addEventListener("click", getTodos);
+document.getElementById("post").addEventListener("click", addTodo);
+document.getElementById("update").addEventListener("click", updateTodo);
+document.getElementById("delete").addEventListener("click", removeTodo);
+document.getElementById("sim").addEventListener("click", getData);
+document.getElementById("headers").addEventListener("click", customHeaders);
+document
+  .getElementById("transform")
+  .addEventListener("click", transformResponse);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("cancel").addEventListener("click", cancelToken);
